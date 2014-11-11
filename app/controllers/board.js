cb041835b1ff10bd7e50018f0dce9aad50757e44
Hristo Atanasov <icetooo@gmail.com>
2014-11-11 21:53:31 +0200

    changed color sheme; responsive design; random numbers generator

--- app/controllers/board.js
+++ app/controllers/board.js
@@ -1,3 +1,28 @@
 spApp.controller('BoardController', ['$scope', function($scope) {
-   $scope.numbers =  [[4, 5, 1, 2], [14, 8, 2, 7], [9, 13, 1, 2], [1, 5, 0, 2]];
+   $scope.numbers =  getRandomBoardNumbers();
+   
+   function getRandomBoardNumbers() {
+      var randomBoardNumbers = [];
+   
+      var allAvailableNumbers = [];
+      for (var i = 0; i < 16; i++) {
+         allAvailableNumbers.push(i + 1);
+      }
+      
+      allAvailableNumbers[Math.floor(Math.random() * allAvailableNumbers.length)] = null;
+   
+      while (allAvailableNumbers.length > 0) {
+         var rowNumbers = [];
+         
+         while (rowNumbers.length < 4) {
+            var randIndex = Math.floor(Math.random() * allAvailableNumbers.length);
+            rowNumbers.push(allAvailableNumbers[randIndex]);
+            allAvailableNumbers.splice(randIndex, 1);
+         }
+         
+         randomBoardNumbers.push(rowNumbers);
+      }
+      
+      return randomBoardNumbers;
+   }
 }]);
---
+++ content/js/dom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+   $(window).resize(function(){
+       setBoardSize();
+   });
+   
+   setBoardSize();
+});
+
+function setBoardSize(){
+   var $ww = $(window).width();
+   var $wh = $(window).height();
+
+   var $squareside = 0;
+
+   if ($ww > $wh) {
+      $squareside = $wh * 0.9;
+   } else {
+      $squareside = $ww * 0.9;
+   }
+    
+   $('#board').css({
+      height: $squareside,
+      width: $squareside
+   }); 
+   
+   var $fontsize = Math.round($squareside / 10);
+   
+   $(document.body).css({
+      fontSize: $fontsize + "px"
+   });
+}
